3d7d33219200c57923621c98b1c243aa167070aa
MiguelDev1 <loormiguel40@gmail.com>
2026-05-10 20:07:31 -0500

    feat: comunicacion Front
    - Se añadio el app para llamados http con el front

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common'; // <-- 1. Importa
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+  });
+
   // <-- 2. Añade esta línea para habilitar la validación global
   app.useGlobalPipes(
     new ValidationPipe({
